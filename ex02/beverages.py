0b2f941184cc38ca98f684b7a097034e582dbe48
Anderson Lima <andersonlimahw@gmail.com>
2023-10-26 13:11:20 -0300

    🆕 [feature] ex02 :  runing beverages

---
+++ ex02/beverages.py
@@ -0,0 +1,55 @@
+class HotBeverage:
+    def __init__(self):
+        self.price = 0.30
+        self.name = "hot beverage"
+
+    def description(self):
+        return "Just some hot water in a cup."
+
+    def __str__(self):
+        return f"name : {self.name}\nprice : {self.price:.2f}\ndescription : {self.description()}\n"
+
+class Coffee(HotBeverage):
+    def __init__(self):
+        super().__init__()
+        self.name = "coffee"
+        self.price = 0.40
+
+    def description(self):
+        return "A coffee, to stay awake."
+
+class Tea(HotBeverage):
+    def __init__(self):
+        super().__init__()
+        self.name = "tea"
+
+class Chocolate(HotBeverage):
+    def __init__(self):
+        super().__init__()
+        self.name = "chocolate"
+        self.price = 0.50
+
+    def description(self):
+        return "Chocolate, sweet chocolate..."
+
+class Cappuccino(HotBeverage):
+    def __init__(self):
+        super().__init__()
+        self.name = "cappuccino"
+        self.price = 0.45
+
+    def description(self):
+        return "Un po’ di Italia nella sua tazza!"
+
+# Test cases
+hot_beverage = HotBeverage()
+coffee = Coffee()
+tea = Tea()
+chocolate = Chocolate()
+cappuccino = Cappuccino()
+
+print("Hot Beverage:\n", hot_beverage)
+print("Coffee:\n", coffee)
+print("Tea:\n", tea)
+print("Chocolate:\n", chocolate)
+print("Cappuccino:\n", cappuccino)
